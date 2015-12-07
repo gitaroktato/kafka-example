91f855fa1e6751caf94d699c66325d554db504fa
U-BUDAPEST\Oresztesz_Margaritis <Oresztesz_Margaritis@EPHUBUDW0845.budapest.epam.com>
2015-12-07 15:51:18 +0100

    Added round-robin with settngs. Needs partitioning

---
+++ round-robin/src/main/java/AConsumer.java
@@ -0,0 +1,52 @@
+import kafka.consumer.Consumer;
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+import kafka.message.MessageAndMetadata;
+import kafka.serializer.Decoder;
+import kafka.serializer.StringDecoder;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+public class AConsumer {
+
+    private static ConsumerConfig createConsumerConfig(String a_zookeeper, String a_groupId) {
+        Properties props = new Properties();
+        props.put("zookeeper.connect", a_zookeeper);
+        props.put("group.id", a_groupId);
+        props.put("zookeeper.session.timeout.ms", "400");
+        props.put("zookeeper.sync.time.ms", "200");
+        props.put("partition.assignment.strategy", "roundrobin");
+        props.put("auto.commit.interval.ms", "1000");
+        return new ConsumerConfig(props);
+    }
+
+    public static void main(String[] args) throws Exception {
+        ConsumerConnector consumer = Consumer.createJavaConsumerConnector(
+                createConsumerConfig(Configuration.ZOOKEEPER_HOST, "roundrobin"));
+        Map<String, Integer> topicCountMap = createTopicCountMap();
+        Decoder<String> decoder = new StringDecoder(null);
+        Map<String, List<KafkaStream<String, String>>> streams =
+                consumer.createMessageStreams(topicCountMap, decoder, decoder);
+        List<KafkaStream<String, String>> justMyStreams = streams.get(Configuration.TOPIC_NAME);
+        justMyStreams.forEach(AConsumer::consume);
+    }
+
+    private static void consume(KafkaStream<String, String> stream) {
+        for (MessageAndMetadata<String, String> msg : stream) {
+            String echo = String.format("Got message: %s, %s, %s",
+                    msg.topic(), msg.key(), msg.message());
+            System.out.println(echo);
+        }
+    }
+
+    private static Map<String, Integer> createTopicCountMap() {
+        Map<String, Integer> result = new HashMap<>();
+        result.put(Configuration.TOPIC_NAME, 1);
+        return result;
+    }
+}
+
---
+++ round-robin/src/main/java/AProducer.java
@@ -0,0 +1,29 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+import java.util.stream.IntStream;
+
+public class AProducer {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", Configuration.KAFKA_HOST);
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        IntStream.range(0, 10).forEach((i) -> {
+                ProducerRecord < String, String > toSend = new ProducerRecord<>(
+                Configuration.TOPIC_NAME, i + "", "message_" + i);
+                producer.send(toSend);
+        });
+        producer.close();
+    }
+}
