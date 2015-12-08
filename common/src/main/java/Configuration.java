0c6e73d075e612ab94dc82ffb8853ecef2681c10
U-BUDAPEST\Oresztesz_Margaritis <Oresztesz_Margaritis@EPHUBUDW0845.budapest.epam.com>
2015-12-08 14:53:49 +0100

    Refactored common config

--- common/src/main/java/Configuration.java
+++ common/src/main/java/Configuration.java
@@ -1,5 +1,5 @@
 public interface Configuration {
     String TOPIC_NAME = "new-topic";
-    String KAFKA_HOST = "192.168.33.10:9092";
+    String KAFKA_HOST = "192.168.33.11:9092,192.168.33.12:9092";
     String ZOOKEEPER_HOST = "192.168.33.10:2181";
 }
--- round-robin/src/main/java/Configuration.java
+++
@@ -1,5 +0,0 @@
-public interface Configuration {
-    String TOPIC_NAME = "new-topic";
-    String KAFKA_HOST = "192.168.33.10:9092";
-    String ZOOKEEPER_HOST = "192.168.33.10:2181";
-}
